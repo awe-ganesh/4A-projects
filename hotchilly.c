b475862e1cd88d45a9c924c29c32ef704edb9d3b
Ganesh <ganeshswaroop8@gmail.com>
2018-03-08 16:15:46 +0530

    Update hotchilly.c

--- hotchilly.c
+++ hotchilly.c
@@ -3,31 +3,34 @@ void main()
 {
 	int chocolate,i,n=1,first,multi,number,l1,l2;
 	printf("Enter the number of chocolates\n");
-	scanf("%d",&chocolate);
-	while((4*n) < chocolate)
+	scanf("%d",&chocolate);     /*Read the number of chocolates*/ 
+	while((4*n) < chocolate)	/*Determines the iterative number 'n' ie. searches for the number 
+					greater than the number of chocolates*/
 		n++;
 		
-	if(chocolate % 4 !=0)
+	if(chocolate % 4 !=0)		/*If the number of chocolates is not a multiple of 4 ,the it enters */ 
 	{
 		printf("Computer");
 		printf("\t\t\tUser\n");
-		multi = (4*n) - 4;
-		first = chocolate - multi;
+		multi = (4*n) - 4;	/*Multi will store the value which is a lesser multiple of 4 
+						ie.(4*3)-4 = 8*/
+		first = chocolate - multi;//First will hild the number of chocolates the computer has taken
 		for(i=0;i<n-1;i++)
 		{
-			chocolate = chocolate - first;
+			chocolate = chocolate - first;//It is the remaining number of chocolates
 			printf("%d",first);
 			printf("\t\t\n");
-			l2 : scanf("\t\t\t\t%d",&number);
-			if(number >3)
+			l2 : scanf("\t\t\t\t%d",&number);//User has to enter this field
+			if(number >3)//This will check that the user will always give within the limits of 1, 2and 3
 				{
 					printf("You can enter either 1 or 2 or maximum 3\n");
 					printf("Take again\n");
 					goto l2;	
 				}
 			printf("\t\t\t\t%d\n",number);
-			chocolate = chocolate - number;
-			first = 4 - number;
+			chocolate = chocolate - number; /*Now the chocolate will hold the number of chocolates which is reemaining
+								after the user hass taken*/
+			first = 4 - number; //We assign first, with the number of chocolate sthe computer has to take
 		}
 			printf("%d",first);
 	}
