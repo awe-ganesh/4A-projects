57483108e23833758df7752e9c3d223c9586cd60
kishore <30802260+kishoreis@users.noreply.github.com>
2018-03-09 10:31:08 +0530

    Update encrypt.c

--- encrypt.c
+++ encrypt.c
@@ -9,7 +9,7 @@ int main()
 	fp1 = fopen("mes.txt","r");
   fp2 = fopen("mesEn.txt","w");
 	char ch;
-	while(fscanf(fp,"%s",&ch)!=EOF)
+	while(fscanf(fp,"%c",&ch)!=EOF)
 	  {
 	     if(ch/10==6){
               res = ((int)floor(6*8.85))+(ch%10);
@@ -24,8 +24,9 @@ int main()
               res = ((int)floor(5*6.37))+(ch%10);
                fprintf(fp2,"%d",res); }
        else mapIt(ch);
-    
   	}
+  fclose(fp1);
+  fclose(fp2);
   return 0;
 }
 
@@ -33,20 +34,20 @@ int mapIt(char ch)
 {
    	switch(ch)
 		{
-      case 48: fprintf("%c",25);	break;
-		case 49: fprintf(fp2,"%c",26);break;
-		case 83 : fprintf(fp2,"%c",67);break;
-		case 62 : fprintf(fp2,"%c",69);	break;
-		case 63 : fprintf(fp2,"%c",69);  break;
-		case 71 : fprintf(fp2,"%c",70);  break;
-		case 70 : fprintf(fp2,"%c",71);	break;
-		case 80 : fprintf(fp2,"%c",73);	break;
-		case 73 : fprintf(fp2,"%c",80);	break;
-		case 67 : fprintf(fp2,"%c",83);	break;
-		case 90 : fprintf(fp2,"%c",89);	break;
-		case 89 : fprintf(fp2,"%c",90);	break;
-		case 32 : fprintf(fp2,"%c",79);	break; 	
-		case 79 : fprintf(fp2,"%c",32);	break; 	
+      case 48: fprintf("%d",25);	break;
+		case 49: fprintf(fp2,"%d",26);break;
+		case 83 : fprintf(fp2,"%d",67);break;
+		case 62 : fprintf(fp2,"%d",69);	break;
+		case 63 : fprintf(fp2,"%d",69);  break;
+		case 71 : fprintf(fp2,"%d",70);  break;
+		case 70 : fprintf(fp2,"%d",71);	break;
+		case 80 : fprintf(fp2,"%d",73);	break;
+		case 73 : fprintf(fp2,"%d",80);	break;
+		case 67 : fprintf(fp2,"%d",83);	break;
+		case 90 : fprintf(fp2,"%d",89);	break;
+		case 89 : fprintf(fp2,"%d",90);	break;
+		case 32 : fprintf(fp2,"%d",79);	break; 	
+		case 79 : fprintf(fp2,"%d",32);	break; 	
 			default:printf("unknown character/error encrypting\n");
               exit(0);
     }
